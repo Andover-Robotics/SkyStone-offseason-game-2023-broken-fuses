ee3230fb9eff4a520ecad63a6129798782814773
anikanagle <nikunagle@gmail.com>
2023-03-31 16:11:07 -0400

    gradle update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
-public class Claw {
+public class Claw{
+    private final ServoEx claw;
+    private final double close=1;
+    private final double open=0;
+    private boolean isOpen=false;
+
+    public Claw(OpMode opMode){
+        claw = (ServoEx) opMode.hardwareMap.servo.get("claw");
+        claw.setPosition(open);
+    }
+    public void open(){
+        claw.setPosition(open);
+        isOpen = true;
+    }
+
+    public void close(){
+        claw.setPosition(close);
+        isOpen = false;
+    }
+
 }
