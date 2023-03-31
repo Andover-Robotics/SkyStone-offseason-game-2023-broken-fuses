aa4eca9131bf6a601ef11d4a08fc30a8ea75d3bf
Nathan Gerow <ngerow12025@gmail.com>
2023-03-31 16:40:29 -0400

    implemnented the Drivechain subsystem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleop.java
@@ -1,10 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import the MechanumDrive class
+import com.arcrobotics.ftclib.drivebase.MecanumDrive;
 
 @TeleOp(name="MainTeleop", group="teleop")
 public class MainTeleop extends LinearOpMode {
@@ -13,18 +14,7 @@ public class MainTeleop extends LinearOpMode {
     public void runOpMode() {
         telementry.addData("Status", "Initialized");
         telementry.update();
-        //make the MechanumDrive
-        DcMotor FR_motor = hardwareMap.get(DcMotor.class, "FR_motor");
-        DcMotor FL_motor = hardwareMap.get(DcMotor.class, "FL_motor");
-        DcMotor BR_motor = hardwareMap.get(DcMotor.class, "BR_motor");
-        DcMotor BL_motor = hardwareMap.get(DcMotor.class, "BL_motor");
-        FR_motor.setDirection(DcMotor.Direction.REVERSE);
-        BR_motor.setDirection(DcMotor.Direction.REVERSE);
 
-//        MechanumDrive
-
-        FL_motor.setDirection(DcMotor.Direction.REVERSE);
-        BL_motor.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
         telementry.addData("Status", "Running");
         telementry.update();
